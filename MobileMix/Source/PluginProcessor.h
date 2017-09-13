c706d9704c50c44d1997b78e0857f0f915a53fd0
Jonathon Racz <jdracz@umich.edu>
2017-09-12 20:19:57 -0400

    Add dynamic_ringbuffer external project Update projects with additional files

--- MobileMix/Source/PluginProcessor.h
+++ MobileMix/Source/PluginProcessor.h
@@ -57,5 +57,7 @@ public:
 
 private:
     //==============================================================================
+    AudioProcessorValueTreeState parameters;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MobileMixAudioProcessor)
 };
