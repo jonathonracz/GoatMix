5f66bb63f3f1432bc3f22caa33e371292e2f661a
Jonathon Racz <jdracz@umich.edu>
2017-12-08 16:03:36 -0500

    Make everything App Store friendly

--- MobileMix/JuceLibraryCode/AppConfig.h
+++ MobileMix/JuceLibraryCode/AppConfig.h
@@ -304,7 +304,7 @@
  #define JucePlugin_Build_Standalone       1
 #endif
 #ifndef  JucePlugin_Enable_IAA
- #define JucePlugin_Enable_IAA             0
+ #define JucePlugin_Enable_IAA             1
 #endif
 #ifndef  JucePlugin_Name
  #define JucePlugin_Name                   "GoatMix"
@@ -343,13 +343,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                1.0.0
+ #define JucePlugin_Version                1.0.3
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x10000
+ #define JucePlugin_VersionCode            0x10003
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "1.0.0"
+ #define JucePlugin_VersionString          "1.0.3"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
